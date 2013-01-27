2e0e39b6daaf671ca6109ee71f52122e3eeb6e4d
Kai Franke <kai@kaifranke.com>
2013-01-26 19:22:05 -0800

    renaming problemm

--- launch_me.sh
+++
@@ -1,9 +0,0 @@
-: ${1?"Usage: $0 /path/to/image.jpg"}
-filename=benchmark_results.csv
-if [ ! -f $filename ];
-then
-	echo "time[ms];keypoints;image path;detector type" > $filename
-fi
-for i in SURF SIFT BRISK ORB
-	do rosrun image_benchmark feature_detection_node _detector_type:=$i  _image_path:=$1 >> $filename
-done
