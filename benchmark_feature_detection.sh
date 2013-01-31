81eea235af71e6a8857d54a6cb8d23b8beb1db84
Kai Franke <kai@kaifranke.com>
2013-01-30 20:48:56 -0800

    Update benchmark_feature_detection.sh
    added another for loop for different detectors and extractors

--- benchmark_feature_detection.sh
+++ benchmark_feature_detection.sh
@@ -9,6 +9,8 @@ then
 fi
 i=`echo "$i + 1" | bc`
 done
-for i in SURF SIFT BRISK ORB
-	do rosrun image_benchmark feature_detection_node _detector_type:="BRISK" _extractor_type:=$i _image_path:=$1 >> $filename
+for j in SURF SIFT BRISK ORB FAST
+	for i in SURF SIFT BRISK ORB FREAK
+		do rosrun image_benchmark feature_detection_node _detector_type:=$j _extractor_type:=$i _image_path:=$1 >> $filename
+	done
 done
